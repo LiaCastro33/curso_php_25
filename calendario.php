3d0448ba5f93312e68235391be677804e3da3eb1
LiadriaCastro33 <liamai2012@gmail.com>
2024-12-17 21:56:23 -0300

    calendario

---
+++ calendario.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+ * criar um algoritmo que simula o funcionamento de um calendario
+ * variaveis:$data = '31/01/2025'
+ * validar a data
+ * validar tambem se o ano é bissexto
+ * dicas: If e else
+ */
+
+
+
+ $data = '31/01/2024';
+
+ $dia = substr($data, 0, 2);
+ $mes = substr($data, 3, 2);
+ $ano = substr($data, 6, 4);
